e48bf267af44af531975bcb543a6e97410ba2b2c
com9425 <yth9425@naver.com>
2020-02-04 18:23:25 +0900

    test

--- app/src/main/java/com/example/iot_team_project/MainActivity.java
+++ app/src/main/java/com/example/iot_team_project/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//test
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
